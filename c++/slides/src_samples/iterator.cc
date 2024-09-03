50d8418b5a48b477675137c7206c4c34dae1dfcb
Oromion <caznaranl@uni.pe>
2024-09-03 11:28:21 -0500

    Initial commit

---
+++ c++/slides/src_samples/iterator.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+
+int main(int argc, char** argv) {
+  std::vector<int> vec;
+  for (int i = 0; i < 10; ++i)
+    vec.push_back(i);
+
+  for (auto it=vec.begin(); it!=vec.end(); ++it)
+    std::cout << *it << " ";
+  std::cout << '\n';
+}
